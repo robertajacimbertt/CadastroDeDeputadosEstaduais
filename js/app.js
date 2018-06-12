872b03cdc2685cdd8bfd6bafae84257a7236f30c
Aluno <rjacimbertt@gmail.com>
2018-06-12 18:29:13 -0300

    nada

--- js/app.js
+++ js/app.js
@@ -1,20 +1,8 @@
+
+
+
 $(document).ready(function(){
 
- $("#calcular").on("click", function(){
-    var peso = $("#peso").val();
-    var altura = $("#altura").val();
-    var resposta;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://andrebordignon.esy.es/php/imc.php?peso=" + peso + "&altura=" + altura); 
-    xhr.send();
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState == 4 && xhr.status == 200){
-        resposta = JSON.parse(xhr.responseText);
-        console.log(resposta);
-         $("body").append("<div id='show'><h1>Resposta:</h1><br><p>Seu IMC é: " + resposta.imc + "</p><p>Análise: " + resposta.mensage + "</p></div>");
-      }
-    }
- });
+alert("OIE");
 
 });
